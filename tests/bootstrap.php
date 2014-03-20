c9c3c6a7a5d477fa66d0018a20430cf561bee37b
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-03-20 14:59:18 +0200

    missed few spaces

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-    define('SOURCE_PATH', dirname( __DIR__ ) . '/src' );
-    define('TEST_PATH', dirname( __DIR__ ) . '/tests' );
-    define('FIXTURE_PATH', TEST_PATH . '/fixtures' );
+    define('SOURCE_PATH', dirname( __DIR__ ) . '/src');
+    define('TEST_PATH', dirname( __DIR__ ) . '/tests');
+    define('FIXTURE_PATH', TEST_PATH . '/fixtures');
 
 
     require __DIR__ . '/../vendor/autoload.php';
