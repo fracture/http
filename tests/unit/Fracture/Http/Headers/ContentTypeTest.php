8d714be20aa8ffe5cbf5459c455d0ef88ed54228
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-07-11 18:19:36 +0300

    tests for moved headers

---
+++ tests/unit/Fracture/Http/Headers/ContentTypeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace Fracture\Http\HEaders;
+
+use Exception;
+use ReflectionClass;
+use PHPUnit_Framework_TestCase;
+
+class ContentTypeTest extends PHPUnit_Framework_TestCase
+{
+
+
+    /**
+     * @covers Fracture\Http\Headers\ContentType::__construct
+     * @covers Fracture\Http\Headers\ContentType::extractData
+     */
+    public function testEmptyInstance()
+    {
+        $instance = new ContentType;
+        $this->assertEquals([], $instance->extractData(''));
+        $this->assertEquals(
+            ['value' => 'application/json'],
+            $instance->extractData('application/json')
+        );
+        $this->assertEquals(
+            ['value' => 'application/json', 'version' => '1'],
+            $instance->extractData('application/json;version=1')
+        );
+        $this->assertEquals(
+            ['value' => 'text/html', 'charset' => 'utf-8'],
+            $instance->extractData('text/html; charset=utf-8')
+        );
+    }
+
+
+    /**
+     * @covers Fracture\Http\Headers\ContentType::__construct
+     * @covers Fracture\Http\Headers\ContentType::prepare
+     * @covers Fracture\Http\Headers\ContentType::contains
+     */
+    public function testPreparedResult()
+    {
+        $instance = new ContentType('text/html');
+        $instance->prepare();
+
+        $this->assertTrue($instance->contains('text/html'));
+        $this->assertFalse($instance->contains('image/png'));
+    }
+
+
+    /**
+     * @covers Fracture\Http\Headers\ContentType::__construct
+     * @covers Fracture\Http\Headers\ContentType::prepare
+     * @covers Fracture\Http\Headers\ContentType::contains
+     * @covers Fracture\Http\Headers\ContentType::setALternativeValue
+     */
+    public function testPreparedResultAterManualAlteration()
+    {
+        $instance = new ContentType('application/json');
+        $instance->setALternativeValue('image/png');
+        $instance->prepare();
+
+        $this->assertTrue($instance->contains('image/png'));
+        $this->assertFalse($instance->contains('text/html'));
+    }
+}
