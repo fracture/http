eb2e917d589c8663ac8b167cf50c93460bbf8db1
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-07-14 19:10:24 +0300

    fixing failing unit test

--- src/Fracture/Http/Parsers/DataForm.php
+++ src/Fracture/Http/Parsers/DataForm.php
@@ -5,4 +5,27 @@ namespace Fracture\Http\Parsers;
 class DataForm
 {
 
+    private $input;
+    private $data = [];
+
+    public function __construct($input)
+    {
+        $this->input = $input;
+    }
+
+
+    public function prepare()
+    {
+
+    }
+
+
+    public function getParameter($name)
+    {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        return null;
+    }
 }
