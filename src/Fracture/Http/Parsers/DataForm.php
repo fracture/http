7fecdd892956bf6bd705063fed234ff041801210
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-07-16 07:27:49 +0300

    fixed data-form parser for simple cases

--- src/Fracture/Http/Parsers/DataForm.php
+++ src/Fracture/Http/Parsers/DataForm.php
@@ -8,10 +8,15 @@ class DataForm
     private $input;
     private $boundry;
 
-    private $data = [];
+    private $factory;
 
-    public function __construct($input, $boundry = null)
+    private $data = [];
+    private $stage = null;
+
+
+    public function __construct(\Fracture\Http\HeaderFactory $factory, $input, $boundry = null)
     {
+        $this->factory = $factory;
         $this->input = $input;
         $this->boundry = $boundry;
     }
@@ -19,23 +24,78 @@ class DataForm
 
     public function prepare()
     {
-        $file = new \SPLFileObject($this->input);
-        $first = true;
-        $separator = '';
+        try {
+            $file = new \SPLFileObject($this->input);
+
+            $this->boundry = $this->guessBoundry($file, $this->boundry);
+
+            foreach ($this->collectParameters($file, $this->boundry) as $key => $value) {
+                $this->data[$key] = $value;
+            }
+
+        } catch (\RuntimeException $e) {
+            return false;
+        }
+
+    }
+
+
+    private function guessBoundry($file, $boundry)
+    {
+        if (false !== strpos($file->current(), $boundry)) {
+            return trim($file->current(), "\r\n");
+        }
+        return null;
+    }
+
+    private function collectParameters(\SPLFileObject $file, $boundry)
+    {
+        $name = null;
+        $value = null;
 
         foreach ($file as $line) {
-            if ($first && strpos($line, $this->boundry)) {
-                $separator = $line;
-                $first = false;
+            if ($line === $boundry .  "--\r\n") {
+                // .. then this is the last line
+                break;
             }
 
-            if ($line === $separator) {
-
+            if ($line === $boundry . "\r\n") {
+                $this->stage = 'headers';
+                continue;
             }
-//            var_Dump($line);
+
+            if ($line === "\r\n") {
+                $this->stage = 'body';
+                continue;
+            }
+
+            switch ($this->stage) {
+                case 'headers':
+                    $entry = $this->readHeader($line);
+                    if ($entry && $entry->getFieldName() === 'Content-Disposition') {
+                        $name = $entry->getAttribute('name');
+                    }
+                    break;
+
+                case 'body':
+                    yield $name => trim($line, "\r\n");
+                    break;
+            }
         }
     }
 
+
+    private function readHeader($line)
+    {
+        $line = trim($line, "\r\n");
+        return $this->factory->create($line);
+    }
+
+
+    private function readBody($line)
+    {
+
+    }
 
     public function getParameter($name)
     {
