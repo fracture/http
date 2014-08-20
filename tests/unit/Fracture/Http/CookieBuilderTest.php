8bb90ccb1bf21a08274f9d3e7a77b11e0419580c
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-08-20 11:55:35 +0300

    finished unit test for cookie builder

--- tests/unit/Fracture/Http/CookieBuilderTest.php
+++ tests/unit/Fracture/Http/CookieBuilderTest.php
@@ -11,6 +11,10 @@ class CookieBuilderTest extends PHPUnit_Framework_TestCase
 {
 
 
+    /**
+     * @covers Fracture\Http\CookieBuilder::__construct
+     * @covers Fracture\Http\CookieBuilder::create
+     */
     public function testCreatingDefaultCookie()
     {
         $instance = new CookieBuilder;
@@ -19,6 +23,10 @@ class CookieBuilderTest extends PHPUnit_Framework_TestCase
     }
 
 
+    /**
+     * @covers Fracture\Http\CookieBuilder::__construct
+     * @covers Fracture\Http\CookieBuilder::create
+     */
     public function testValueAndNameOfCreateCookie()
     {
         $instance = new CookieBuilder;
@@ -28,6 +36,10 @@ class CookieBuilderTest extends PHPUnit_Framework_TestCase
     }
 
 
+    /**
+     * @covers Fracture\Http\CookieBuilder::__construct
+     * @covers Fracture\Http\CookieBuilder::create
+     */
     public function testCompleteDefaultSetupForCookie()
     {
         $instance = new CookieBuilder;
@@ -42,15 +54,33 @@ class CookieBuilderTest extends PHPUnit_Framework_TestCase
     }
 
 
+    /**
+     * @covers Fracture\Http\CookieBuilder::__construct
+    * @covers Fracture\Http\CookieBuilder::create
+    * @covers Fracture\Http\CookieBuilder::setParameter
+     */
     public function testResultWhenAlteringBuildersPamameters()
     {
         $instance = new CookieBuilder;
+        $instance->setParameter('fake', 1);
+
         $cookie = $instance->create('foo', 'bar');
         $this->assertEquals([
             'expires' => null,
             'path' => '/',
             'domain' => '',
             'secure' => false,
+            'httpOnly' => true,
+        ], $cookie->getParameters());
+
+        $instance->setParameter('secure', true);
+
+        $cookie = $instance->create('foo', 'bar');
+        $this->assertEquals([
+            'expires' => null,
+            'path' => '/',
+            'domain' => '',
+            'secure' => true,
             'httpOnly' => true,
         ], $cookie->getParameters());
     }
