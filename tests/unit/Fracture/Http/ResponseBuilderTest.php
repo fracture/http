b07188ddc9a6ad57fc0b68aa075cc5e8a2d5f9df
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-09-03 09:57:47 +0300

    fixing the stupid: wrong implementations for cookie handling

--- tests/unit/Fracture/Http/ResponseBuilderTest.php
+++ tests/unit/Fracture/Http/ResponseBuilderTest.php
@@ -13,40 +13,10 @@ class ResponseBuilderTest extends PHPUnit_Framework_TestCase
     /**
      * @covers Fracture\Http\ResponseBuilder::__construct
      * @covers Fracture\Http\ResponseBuilder::create
-     * @covers Fracture\Http\ResponseBuilder::applyCookies
      */
     public function testResponseCreatedWithNoCookies()
     {
-        $requestMock = $this->getMock('Fracture\Http\Request', ['getAllCookies']);
-        $requestMock->expects($this->once())
-                    ->method('getAllCookies')
-                    ->will($this->returnValue([]));
-
-
-        $instance = new ResponseBuilder($requestMock);
-        $this->assertInstanceOf('Fracture\Http\Response', $instance->create());
-    }
-
-
-    /**
-     * @covers Fracture\Http\ResponseBuilder::__construct
-     * @covers Fracture\Http\ResponseBuilder::create
-     * @covers Fracture\Http\ResponseBuilder::applyCookies
-     */
-    public function testResponseCreatedWithSomeCookies()
-    {
-        $cookieMock = $this->getMock('Fracture\Http\Cookie', ['getName'], [], '', false);
-        $cookieMock->expects($this->once())
-                   ->method('getName')
-                   ->will($this->returnValue('name'));
-
-
-
-        $requestMock = $this->getMock('Fracture\Http\Request', ['getAllCookies']);
-        $requestMock->expects($this->once())
-                    ->method('getAllCookies')
-                    ->will($this->returnValue(['name' => $cookieMock]));
-
+        $requestMock = $this->getMock('Fracture\Http\Request');
 
         $instance = new ResponseBuilder($requestMock);
         $this->assertInstanceOf('Fracture\Http\Response', $instance->create());
@@ -58,15 +28,11 @@ class ResponseBuilderTest extends PHPUnit_Framework_TestCase
      * @covers Fracture\Http\ResponseBuilder::create
      * @covers Fracture\Http\ResponseBuilder::setAvailableContentTypes
      *
-     * @covers Fracture\Http\ResponseBuilder::applyCookies
      * @covers Fracture\Http\ResponseBuilder::attemptSettingContentType
      */
     public function testSimpleExpectedTypesWithMissingAcceptHeader()
     {
-        $requestMock = $this->getMock('Fracture\Http\Request', ['getAllCookies', 'getAcceptHeader']);
-        $requestMock->expects($this->once())
-                    ->method('getAllCookies')
-                    ->will($this->returnValue([]));
+        $requestMock = $this->getMock('Fracture\Http\Request', ['getAcceptHeader']);
         $requestMock->expects($this->once())
                     ->method('getAcceptHeader')
                     ->will($this->returnValue(null));;
@@ -89,7 +55,6 @@ class ResponseBuilderTest extends PHPUnit_Framework_TestCase
      * @covers Fracture\Http\ResponseBuilder::create
      * @covers Fracture\Http\ResponseBuilder::setAvailableContentTypes
      *
-     * @covers Fracture\Http\ResponseBuilder::applyCookies
      * @covers Fracture\Http\ResponseBuilder::attemptSettingContentType
      * @covers Fracture\Http\ResponseBuilder::applyContentTypeHeader
      */
@@ -102,10 +67,7 @@ class ResponseBuilderTest extends PHPUnit_Framework_TestCase
                    ->method('contains')
                    ->will($this->onConsecutiveCalls(false, true));
 
-        $requestMock = $this->getMock('Fracture\Http\Request', ['getAllCookies', 'getAcceptHeader']);
-        $requestMock->expects($this->once())
-                    ->method('getAllCookies')
-                    ->will($this->returnValue([]));
+        $requestMock = $this->getMock('Fracture\Http\Request', ['getAcceptHeader']);
         $requestMock->expects($this->once())
                     ->method('getAcceptHeader')
                     ->will($this->returnValue($headerMock));;
